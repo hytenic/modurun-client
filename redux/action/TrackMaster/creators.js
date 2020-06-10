e55acbdd856f6d8178b34904f3dd6f3b498cfe41
monthem <lpoeh01@gmail.com>
2020-06-10 22:53:34 +0900

    ADD: added addTrack action for TrackMaster

--- redux/action/TrackMaster/creators.js
+++ redux/action/TrackMaster/creators.js
@@ -1,3 +1,4 @@
+/* eslint-disable arrow-body-style */
 /* eslint-disable import/prefer-default-export */
 import * as types from './types';
 
@@ -5,5 +6,12 @@ export const setUserLocation = ({ curPosition, curPosCamera }) => {
   return {
     type: types.UPDATE_USERLOCATION,
     payload: { curPosition, curPosCamera },
+  };
+};
+
+export const addTrack = (track) => {
+  return {
+    type: types.ADD_TRACK,
+    payload: track,
   };
 };
