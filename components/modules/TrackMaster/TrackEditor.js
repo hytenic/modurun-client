bc869f9ff55e1706c788834d530d2e1604046b9e
monthem <lpoeh01@gmail.com>
2020-06-10 22:39:43 +0900

    ADD | MODIFY: added completeEdit interface partially, modified mapview to start on user location. - added completeEdit interface partially   * added completeEdit Button and applied style to hide when user is typing.   * added title input and applied style to hide  when user is not typing. - modified mapview to start on user location   * added goToCurrentLocation function which using expo-location to get user location.   * appended goToCurrentLocation function to MapView onMapReady event.

--- components/modules/TrackMaster/TrackEditor.js
+++ components/modules/TrackMaster/TrackEditor.js
@@ -1,24 +1,31 @@
+/* eslint-disable max-len */
 /* eslint-disable react/jsx-props-no-spreading */
-import React, { useState } from 'react';
-import { View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {
+  View, Text, KeyboardAvoidingView, Keyboard, BackHandler, Alert,
+} from 'react-native';
 import MapView from 'react-native-maps';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
+import { TouchableOpacity, TouchableHighlight, TextInput } from 'react-native-gesture-handler';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import MaterialCommunity from 'react-native-vector-icons/MaterialCommunityIcons';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Track from './Track';
 import styles from './style';
 import utils from './utils';
 import MapMarker from './MapMarker';
-
+import * as actions from '../../../redux/action/TrackMaster/creators';
 
 const Icon = {
   FontAwesome,
   MaterialCommunity,
 };
 
-const TrackMaster = ({ curPosCamera }) => {
+const TrackEditor = ({ curPosCamera, addTrack }) => {
   const [mapWidth, setMapWidth] = useState(99);
+  const [titleInputStyle, setTitleInputStyle] = useState({});
   // const [errorMsg, setErrorMsg] = useState(null);
   const [markable, setMarkable] = useState(false);
   const [markers, setMarkers] = useState([]);
@@ -27,15 +34,82 @@ const TrackMaster = ({ curPosCamera }) => {
   const [routes, setRoutes] = useState([]);
   const [routeHistory, setRouteHistory] = useState([]);
   const [markerHistory, setMarkerHistory] = useState([]);
+  const [initialCamera, setInitialCamera] = useState(undefined);
 
   const [layoutInfo, setLayoutInfo] = useState(null);
   const [regionInfo, setRegionInfo] = useState({ start: {} });
+
+  const [toolBarParentWidth, setToolBarParentWidth] = useState(undefined);
+
+  const [completeVisible, setCompleteVisibility] = useState(false);
+  const [typingText, setTypingStatus] = useState(false);
+
+  const [exit, setExit] = useState(false);
+
+  const setTypingFalse = () => setTypingStatus(false);
+  const setTypingTrue = () => setTypingStatus(true);
+
+  const setCompleteInvisible = () => {
+    setCompleteVisibility(false);
+    return true;
+  };
+
+  const setCompleteVisible = () => {
+    setCompleteVisibility(true);
+  };
+
+  async function goToCurrentLocation() {
+    const {
+      status,
+      // permissions,
+    } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      // setErrorMsg('Permission to access location was denied');
+    }
+    const position = await Location.getCurrentPositionAsync({});
+    if (exit) throw Error('Editor has been closed');
+    const { coords } = position;
+    const camera = undefined || {
+      center: {
+        latitude: coords.latitude,
+        longitude: coords.longitude,
+      },
+      altitude: coords.altitude,
+      pitch: 0,
+      heading: 0,
+      zoom: 15,
+    };
+    setInitialCamera(camera);
+  }
+
+  const toggleCompleteVisible = () => setCompleteVisibility(!completeVisible);
+
+  useEffect(() => {
+    Keyboard.addListener('keyboardDidShow', setTypingTrue);
+    Keyboard.addListener('keyboardDidHide', setTypingFalse);
+    BackHandler.addEventListener('hardwareBackPress', setCompleteInvisible);
+    return () => {
+      setExit(true);
+      Keyboard.removeListener('keyboardDidShow', setTypingTrue);
+      Keyboard.removeListener('keyboardDidHide', setTypingFalse);
+      BackHandler.removeEventListener('hardwareBackPress', setCompleteInvisible);
+    };
+  }, []);
 
   // Init mapView(For activating follow user button)
   const updateMapStyle = () => {
     setMapWidth('100%');
   };
 
+  const updateTitleInputStyle = () => {
+    setTitleInputStyle({
+      paddingLeft: 10,
+    });
+  };
+
+  const updateToolBarParentWidth = (e) => {
+    setToolBarParentWidth(e.nativeEvent.layout.width);
+  };
 
   const pushRouteHistory = (newRoutes) => {
     setRouteHistory([...routeHistory, newRoutes]);
@@ -200,49 +274,100 @@ const TrackMaster = ({ curPosCamera }) => {
 
   const mapViewProps = {
     rotateEnabled: false,
+    initialCamera: curPosCamera || initialCamera,
     onRegionChangeComplete: (region) => setRegionInfo(region),
     onLayout: (e) => setLayoutInfo(e.nativeEvent.layout),
-    style: [styles.mapStyle, { width: mapWidth }],
+    style: [styles.mapStyle, { width: mapWidth, flex: typingText ? 0 : 1 }],
     showsUserLocation: true,
     onMapReady: () => {
       updateMapStyle();
+      goToCurrentLocation();
     },
   };
 
+  const toolBarIconSize = (() => {
+    if (!toolBarParentWidth) return 30;
+    if (toolBarParentWidth >= 300) {
+      return 30;
+    }
+    return toolBarParentWidth / 10;
+  })();
+
+  const toolBarIconStyle = (backgroundColor) => styles.toolBarIcon(backgroundColor, toolBarIconSize);
+
   return (
-    <View style={styles.container}>
-      <View onTouchMove={trackTouch} onTouchEnd={onMarkTouchEnd} style={styles.toolBar}>
-        <TouchableOpacity
-          onLayout={initTouchPos}
-          onPressIn={startMark}
-          style={styles.toolBarIcon()}
-        >
-          <Icon.FontAwesome name="map-marker" size={30} color="crimson" />
-        </TouchableOpacity>
-        <TouchableOpacity onPress={clearMarker} style={styles.toolBarIcon()}>
-          <Icon.FontAwesome name="trash" size={30} color="grey" />
-        </TouchableOpacity>
-        <TouchableOpacity onPress={completeTrack} style={styles.toolBarIcon()}>
-          <Icon.FontAwesome name="magic" size={30} color="dodgerblue" />
-        </TouchableOpacity>
-        <TouchableOpacity onPress={deletePrevMarker} style={styles.toolBarIcon()}>
-          <Icon.FontAwesome name="undo" size={30} color="black" />
-        </TouchableOpacity>
-        <View style={styles.addMarker(touchPos, correctHiding)}>
-          <Icon.FontAwesome name="map-marker" style={{ display: markable ? 'flex' : 'none' }} size={30} color="#ff385f" />
+    <View onLayout={updateToolBarParentWidth} style={styles.container}>
+      <View>
+
+        <View>
+          <View onTouchMove={trackTouch} onTouchEnd={onMarkTouchEnd} style={styles.toolBar}>
+            <View style={{ flexDirection: 'column' }}>
+              <TouchableOpacity onLayout={initTouchPos} onPressIn={startMark} style={toolBarIconStyle()}>
+                <Icon.FontAwesome name="map-marker" size={toolBarIconSize} color="crimson" />
+              </TouchableOpacity>
+              <TouchableOpacity onPress={clearMarker} style={toolBarIconStyle()}>
+                <Icon.FontAwesome name="trash" size={toolBarIconSize} color="grey" />
+              </TouchableOpacity>
+              <TouchableOpacity onPress={completeTrack} style={toolBarIconStyle()}>
+                <Icon.FontAwesome name="magic" size={toolBarIconSize} color="dodgerblue" />
+              </TouchableOpacity>
+              <TouchableOpacity onPress={deletePrevMarker} style={toolBarIconStyle()}>
+                <Icon.FontAwesome name="undo" size={toolBarIconSize} color="black" />
+              </TouchableOpacity>
+            </View>
+            <View style={styles.addMarker(touchPos, correctHiding)}>
+              <Icon.FontAwesome name="map-marker" style={{ display: markable ? 'flex' : 'none' }} size={30} color="#ff385f" />
+            </View>
+          </View>
         </View>
+        <View style={styles.completeButtonContainer(!completeVisible, layoutInfo ? layoutInfo.height - 60 : 300)}>
+          <TouchableOpacity onPress={toggleCompleteVisible} style={[toolBarIconStyle('#5bc253'), {display: completeVisible ? 'none' : 'flex'}]}>
+            <Icon.FontAwesome name="check" size={toolBarIconSize} color="white" />
+          </TouchableOpacity>
+        </View>
+        {/* This is a marker that is following touch */}
       </View>
-      <MapView camera={curPosCamera || undefined} {...mapViewProps}>
+
+      <MapView {...mapViewProps}>
         {renderMarkers()}
         <Track data={routes} isPreview visibleMarker={false} />
       </MapView>
+
+      <KeyboardAvoidingView style={styles.titleInputContainer(completeVisible, typingText)}>
+        <TextInput onLayout={updateTitleInputStyle} style={[styles.titleInput, titleInputStyle]} placeholder="트랙의 이름을 지어주세요" />
+        <TouchableOpacity style={styles.editCompleteButton}>
+          <Text style={{ color: 'white' }}>제작 완료</Text>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 };
 
 
+TrackEditor.defaultProps = {
+  curPosCamera: PropTypes.object.isRequired,
+};
+
+TrackEditor.propTypes = {
+  curPosCamera: PropTypes.shape({
+    altitude: PropTypes.number.isRequired,
+    center: PropTypes.shape({
+      latitude: PropTypes.number.isRequired,
+      longitude: PropTypes.number.isRequired,
+    }),
+    heading: PropTypes.number.isRequired,
+    pitch: PropTypes.number.isRequired,
+    zoom: PropTypes.number.isRequired,
+  }),
+  addTrack: PropTypes.func.isRequired,
+};
+
 const mapStateToProps = (state) => ({
   curPosCamera: state.trackMaster.userLocation.curPosCamera,
 });
 
-export default connect(mapStateToProps, null)(TrackMaster);
+const mapDispatchToProps = {
+  addTrack: actions.addTrack,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(TrackEditor);
