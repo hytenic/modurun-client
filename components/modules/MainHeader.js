66285b7b802b968caac6794bb789a14bacabc9de
Starcush <hacker0419@gmail.com>
2020-06-12 00:39:05 +0900

    ADD sidebar component
     - installed @react-navigation/drawer npm to make sidebar
     - if click sidebar image it shows sidebar

--- components/modules/MainHeader.js
+++ components/modules/MainHeader.js
@@ -1,14 +1,52 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity, Image, Alert } from 'react-native';
+import SideBar from './SideBar';
 
-const MainHeader = () => {
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 5,
+  },
+  header: {
+    flex: 1,
+    backgroundColor: 'yellow',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+  },
+  main: {
+    flex: 10,
+    backgroundColor: 'green',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+
+const MainHeader = ({ navigation }) => {
+  const toggleSideBar = () => {
+    navigation.openDrawer();
+  };
+
   return (
-    <View>
-      <Text></Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity onPress={() => {
+          toggleSideBar();
+        }}
+        >
+          <Image
+            source={{ uri: 'https://reactnativecode.com/wp-content/uploads/2018/04/hamburger_icon.png' }}
+            style={{ width: 25, height: 25, marginLeft: 15 }}
+          />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.main}>
+        <Text>메인</Text>
+      </View>
     </View>
-  )
-}
+  );
+};
 
 export default MainHeader
 
-const styles = StyleSheet.create({})
+
