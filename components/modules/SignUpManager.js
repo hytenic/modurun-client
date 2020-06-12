5722a9ed4b77898446fe4f4af5f148c94f840af4
Starcush <hacker0419@gmail.com>
2020-06-12 13:12:14 +0900

    MODIFY modified signup manager css

--- components/modules/SignUpManager.js
+++ components/modules/SignUpManager.js
@@ -1,10 +1,42 @@
 import React, { useState } from 'react';
 import {
-  StyleSheet, Text, View, TextInput, Button,
+  StyleSheet, Text, View, TextInput, TouchableOpacity, Alert,
 } from 'react-native';
 const axios = require('axios');
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    flex: 0.2,
+    // backgroundColor: 'yellow',
+    justifyContent: 'center',
+  },
+  headerText: {
+    fontSize: 30,
+    textAlign: 'center',
+  },
+  body: {
+    flex: 0.7,
+    // backgroundColor: 'green',
+    justifyContent: 'center',
+  },
+  footer: {
+    flex: 0.1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  inputArea: {
+    flex: 0.55,
+    // backgroundColor: 'green',
+    justifyContent: 'center',
+  },
+  buttonArea: {
+    flex: 0.45,
+    // backgroundColor: 'blue',
+    alignItems: 'flex-end',
+  },
   textInput: {
     marginTop: 20,
     marginLeft: 10,
@@ -14,25 +46,15 @@ const styles = StyleSheet.create({
     borderRadius: 8,
   },
   signUpButton: {
-    margin: 20,
+    backgroundColor: '#FADAD8',
+    width: 100,
+    height: 30,
+    marginRight: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 8,
   },
 });
-
-const EmailInputComponet = ({ email }) => {
-  return (
-    <TextInput
-      style={styles.textInput}
-      placeholder="이메일을 입력해 주세요."
-      defaultValue={email}
-      onTouchStart={() => {
-        setJustAfterEmail(true);
-      }}
-      onChangeText={(text) => {
-        setEmail(text);
-      }}
-    />
-  );
-};
 
 // eslint-disable-next-line react/prop-types
 const SignUpManager = ({ navigation }) => {
@@ -45,61 +67,84 @@ const SignUpManager = ({ navigation }) => {
 
   // eslint-disable-next-line func-names
   const emailDuplication = function (emailCheck) {
+    Alert.alert("이메일 체크");
     axios.post(`${bareURL}/users/exist`, {
       emailCheck,
     }).then((res) => {
       if (res.statusCode === 201) {
-        alert('중복된 이메일 입니다.');
+        Alert.alert('중복된 이메일 입니다.');
       }
     });
   };
 
   return (
-    <View>
-      <Text style={{ alignItems: 'center' }}>회원가입</Text>
-      <EmailInputComponet email={email} />
-      <TextInput
-        style={styles.textInput}
-        placeholder="비밀번호를 입력해 주세요."
-        defaultValue={password}
-        secureTextEntry
-        onTouchStart={() => {
-          if (justAfterEmail) {
-            emailDuplication(email);
-            setJustAfterEmail(false);
-          }
-        }}
-        onChangeText={(pw) => {
-          setPassword(pw);
-        }}
-      />
-      <TextInput
-        style={styles.textInput}
-        placeholder="비밀번호를 다시 입력해 주세요."
-        defaultValue={confirmPassword}
-        secureTextEntry
-        onChangeText={(confirm) => {
-          setConfirmPassword(confirm);
-        }}
-      />
-      <Button
-        style={styles.signUpButton}
-        title="회원가입"
-        onPress={() => {
-          if (password === confirmPassword) {
-            alert('입력한 두 비밀번호가 같기 때문에 회원가입을 요청합니다.');
-            axios.post(`${bareURL}/users/signup`, {
-              email,
-              password,
-            }).then((res) => console.log(res));
-          } else {
-            alert('비밀번호가 다릅니다.');
-          }
-        }}
-      />
-      <Text>이미 계정이 있으신가요?</Text>
-      {/* eslint-disable-next-line react/prop-types */}
-      <Text onPress={() => navigation.navigate('SignInManager')}>로그인</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>모두의 런 회원가입</Text>
+      </View>
+      <View style={styles.body}>
+        <View style={styles.inputArea}>
+          <TextInput
+            style={styles.textInput}
+            placeholder="이메일을 입력해 주세요."
+            defaultValue={email}
+            onTouchStart={() => {
+              setJustAfterEmail(true);
+            }}
+            onChangeText={(text) => {
+              setEmail(text);
+            }}
+          />
+          <TextInput
+            style={styles.textInput}
+            placeholder="비밀번호를 입력해 주세요."
+            defaultValue={password}
+            secureTextEntry
+            onTouchStart={() => {
+              if (justAfterEmail) {
+                emailDuplication(email);
+                setJustAfterEmail(false);
+              }
+            }}
+            onChangeText={(pw) => {
+              setPassword(pw);
+            }}
+          />
+          <TextInput
+            style={styles.textInput}
+            placeholder="비밀번호를 다시 입력해 주세요."
+            defaultValue={confirmPassword}
+            secureTextEntry
+            onChangeText={(confirm) => {
+              setConfirmPassword(confirm);
+            }}
+          />
+        </View>
+        <View style={styles.buttonArea}>
+          <TouchableOpacity
+            style={styles.signUpButton}
+            title="회원가입"
+            onPress={() => {
+              if (password === confirmPassword) {
+                Alert.alert('입력한 두 비밀번호가 같기 때문에 회원가입을 요청합니다.');
+                axios.post(`${bareURL}/users/signup`, {
+                  email,
+                  password,
+                }).then((res) => console.log(res));
+              } else {
+                Alert.alert('비밀번호가 다릅니다.');
+              }
+            }}
+          >
+            <Text>회원가입</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View style={styles.footer}>
+        <Text>이미 계정이 있으신가요?</Text>
+        <Text>    </Text>
+        <Text style={{color: 'blue'}} onPress={() => navigation.navigate('SignInManager')}>로그인</Text>
+      </View>
     </View>
   );
 };
