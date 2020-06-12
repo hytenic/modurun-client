8fc1e8f95d88b4023b8ac9e5fb1d7d501b44d363
monthem <lpoeh01@gmail.com>
2020-06-12 13:57:17 +0900

    ADD: ScheduleTrackViewer has been added for serving schedules. - Callout is rendered for each track. - Things happens when a marker is pressed are handled on "onMarkerPress"   * set track that is related to the marker as a selected track, so that other tracks are invisible.   * append related track data to "onTrackSelected" callback.

--- components/modules/TrackMaster/TrackViewer.js
+++ components/modules/TrackMaster/TrackViewer.js
@@ -2,11 +2,17 @@
 import { connect } from 'react-redux';
 import React, { useState } from 'react';
 import * as Location from 'expo-location';
-import MapView from 'react-native-maps';
+import MapView, { Polyline, Marker, Callout } from 'react-native-maps';
+import { Text, View } from 'react-native';
 import styles from './style';
+import Track from './Track';
+import utils from './utils';
 
-const TrackViewer = ({ curPosCamera }) => {
+const { paleColor } = utils;
+
+const TrackViewer = ({ curPosCamera, onRegionChange, onTrackSelected, tracks }) => {
   const [mapWidth, setMapWidth] = useState('99%');
+  const [selectedTrack, setSelectedTrack] = useState(null);
   const updateMapStyle = () => {
     setMapWidth('100%');
   };
@@ -22,13 +28,84 @@ const TrackViewer = ({ curPosCamera }) => {
     },
   };
 
-  const renderIfHasLocation = () => {
-    if (!curPosCamera) return <></>; // spinner should be here;
-    return <MapView initialCamera={curPosCamera} {...mapViewProps} />;
+  const onMarkerPress = (track) => {
+    const {
+      trackTitle,
+      origin,
+      destination,
+      trackLength,
+    } = track;
+    setSelectedTrack(track);
+    onTrackSelected({
+      trackTitle,
+      origin,
+      destination,
+      trackLength,
+    });
+  };
+
+  if (!curPosCamera) return <></>; // spinner should be here;
+
+  const renderCalloutProps = (calloutProps) => {
+    const decideMarginBottom = (index) => (index < calloutProps.length - 1 ? 10 : 0);
+    return calloutProps.map(([key, value, keyBackgroundColor, keyColor, valueBackgroundColor, valueColor], i) => (
+      <View style={[styles.callloutPropRow, { marginBottom: decideMarginBottom(i) }]}>
+        <Text style={styles.callloutPropKey(keyBackgroundColor, keyColor)}>{key}</Text>
+        <Text style={styles.calloutPropValue(valueBackgroundColor, valueColor)}>{value}</Text>
+      </View>
+    ));
+  };
+
+  const colors = {
+    from: 'rgba(148, 87, 255, 1)',
+    span: 'rgba(74, 167, 255, 1)',
+    trackTitle: 'rgba(42, 176, 82, 1)',
+    trackLength: 'rgba(147, 176, 42, 1)',
+    participants: 'rgba(247, 149, 57, 1)',
+    maleSpan: 'rgba(169, 39, 179, 1)',
+    femaleSpan: 'rgba(41, 201, 255, 1)',
+  };
+
+  const callOut = (track) => {
+    const {
+      trackTitle,
+      trackLength,
+      origin,
+      destination,
+    } = track;
+
+    const calloutProps = [
+      ['길이', `${trackLength}m`, colors.trackLength, 'white', paleColor(colors.trackLength)],
+      ['시간(남)', '계산해야 됨', colors.maleSpan, 'white', paleColor(colors.maleSpan)],
+      ['시간(여)', '계산해야 됨', colors.femaleSpan, 'white', paleColor(colors.femaleSpan)],
+    ];
+
+    return (
+      <Callout tooltip>
+        <View style={styles.calloutView}>
+          <Text style={styles.calloutTitle}>{trackTitle}</Text>
+          <View style={styles.calloutSeperator} />
+          <View style={{ alignContent: 'space-between' }}>
+            {renderCalloutProps(calloutProps)}
+          </View>
+        </View>
+      </Callout>
+    );
   };
 
   return (
-    renderIfHasLocation()
+    <MapView onRegionChange={onRegionChange} initialCamera={curPosCamera} {...mapViewProps}>
+      {tracks.map((track) => (
+        <Track
+          key={track.trackTitle}
+          visible={track === selectedTrack}
+          callOut={callOut(track)}
+          onMarkerPress={onMarkerPress}
+          track={track}
+          data={track}
+        />
+      ))}
+    </MapView>
   );
 };
 
