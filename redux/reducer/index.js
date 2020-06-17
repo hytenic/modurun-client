5276317d533a11abcf3bf66f42ed245d0a5fa41b
monthem <lpoeh01@gmail.com>
2020-06-17 13:00:33 +0900

    ADD: Added singleTrackViewer reducer for managing targetTrackinfo.

--- redux/reducer/index.js
+++ redux/reducer/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import myPageReducer from './myPage';
 import trackMasterReducer from './TrackMaster';
+import singleTrackViewerReducer from './SingleTrackViewer';
 
 const rootReducer = combineReducers({
   myPage: myPageReducer,
   trackMaster: trackMasterReducer,
+  SingleTrackViewer: singleTrackViewerReducer,
 });
 
 export default rootReducer;
