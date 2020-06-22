b9717a81648c92a8ff29a23d63a6b6ba2b3c3c38
monthem <lpoeh01@gmail.com>
2020-06-23 00:22:20 +0900

    ADD: added room for fetching previous messages by using reduxStore.chatRoom.offset[scheduleId]

--- redux/reducer/ChatRoom.js
+++ redux/reducer/ChatRoom.js
@@ -2,11 +2,13 @@ import types from '../action/ChatRoom/types';
 
 const initialState = {
   data: {},
+  offset: {},
 };
 
 const reducer = (state = initialState, action) => {
   const newState = {
     data: { ...state.data },
+    offset: { ...state.offset },
   };
 
   if (action.type === types.UPDATE_CHAT) {
@@ -22,6 +24,11 @@ const reducer = (state = initialState, action) => {
   if (action.type === types.ADD_MESSAGES_TO_HEAD) {
     const { scheduleId, prevChat } = action.payload;
     newState.data[scheduleId] = [...prevChat.concat(state.data[scheduleId])];
+  }
+
+  if (action.type === types.SET_CHAT_OFFSET) {
+    const { newOffset, scheduleId } = action;
+    newState.offset[scheduleId] = newOffset;
   }
 
   return newState;
