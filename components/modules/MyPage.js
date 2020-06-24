d956733cc604dbad8096ce0aec100a3abf2a328b
lmt <1571min@naver.com>
2020-06-24 15:40:22 +0900

    MODIFY: mypage view

--- components/modules/MyPage.js
+++ components/modules/MyPage.js
@@ -8,6 +8,27 @@ import TrackRecord from './MyPage/TrackRecord';
 import dummySchedules from './TrackMaster/dummyData/dummySchedules.json';
 import MaskedView from '@react-native-community/masked-view';
 
+const styles = StyleSheet.create({
+  compactTitleStyle: {
+    fontSize: 20,
+    color: 'white',
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+  userInfoContainerStyle: {
+    flex: 0.7,
+    margin: 20,
+    marginVertical: 10,
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 3, height: 3 },
+    shadowOpacity: 0.4,
+    shadowRadius: 3,
+    elevation: 50,
+    backgroundColor: 'white',
+  },
+});
+
 const MyPage = (props) => {
   const dummyUser = {
     name: '바보똥개',
@@ -22,10 +43,10 @@ const MyPage = (props) => {
   });
 
   return (
-    <ScrollView style={{ flex: 1 }}>
-      <Text style={{ fontSize: 20, margin: 20, marginVertical: 10 }}>My 모두런</Text>
-      <View style={{ margin: 20, marginVertical: 10, borderRadius: 10, elevation: 2, backgroundColor: 'white' }}>
-        <View style={{flexDirection: 'row', alignItems: 'center', borderBottomWidth: 1, borderBottomColor: 'rgba(0,0,0,0.1)', padding: 10, justifyContent: 'space-between' }}>
+    <ScrollView style={{ flex: 3, backgroundColor: '#1E90FF'}}>
+      <View style={styles.userInfoContainerStyle}>
+        <Text style={{ fontSize: 25, margin: 20, marginVertical: 10 }}>My 모두런</Text>
+        <View style={{flexDirection: 'row', alignItems: 'center', borderBottomWidth: 1, borderBottomColor: 'rgba(0,0,0,0.1)', justifyContent: 'space-between' }}>
           <View style={{flexDirection: 'row', alignItems: 'center' }}>
             <View style={{backgroundColor: 'lightgrey', borderRadius: 100, padding: 2, margin: 10 }}>
               <MaskedView style={{backgroundColor: 'transparent' }} maskElement={<View style={{backgroundColor: 'black', width: 40, height: 40, borderRadius: 100 }} />}>
@@ -34,18 +55,18 @@ const MyPage = (props) => {
             </View>
             <Text style={{ fontSize: 17 }}>{`${dummyUser.name}`}</Text>
           </View>
-          <TouchableHighlight underlayColor="rgba(0,0,0,0.1)" onPressIn={()=>{}} style={{margin: 10, borderWidth: 1, borderColor: 'rgba(0,0,0,0.3)', padding: 5, paddingHorizontal: 10, borderRadius: 5 }}>
-            <Text style={{fontSize: 12}}>내 정보 관리</Text>
+          <TouchableHighlight underlayColor="rgba(0,0,0,0.1)" onPressIn={()=>{}} style={{margin: 10, backgroundColor: '#03D6A7', padding: 5, paddingHorizontal: 10, borderRadius: 5 }}>
+            <Text style={{fontSize: 12, color: 'white', fontWeight: 'bold'}}>내 정보 관리</Text>
           </TouchableHighlight>
         </View>
-        <View style={{padding: 20, flexDirection: 'row', borderBottomColor:'rgba(0,0,0,0.1)', borderBottomWidth: 1 }}>
-          <Icon name="envelope" size={20} color="lightgrey" style={{marginRight: 10}} />
+        <View style={{padding: 15, flexDirection: 'row', borderBottomColor:'rgba(0,0,0,0.1)', borderBottomWidth: 1 }}>
+          <Icon name="envelope" size={15} color="#1E90FF" style={{marginRight: 10}} />
           <Text>{`${dummyUser.email}`}</Text>
         </View>
         <TouchableHighlight onPressIn={()=>{}} underlayColor="rgba(0,0,0,0.1)" style={{borderBottomLeftRadius: 10, borderBottomRightRadius: 10}}>
-          <View style={{padding: 20, flexDirection: 'row', justifyContent: 'space-between' }}>
+          <View style={{padding: 15, flexDirection: 'row', justifyContent: 'space-between' }}>
             <View style={{flexDirection: 'row' }}>
-              <Icon name="thumbs-up" size={20} color="lightgrey" style={{marginRight: 10}} />
+              <Icon name="thumbs-up" size={15} color="#1E90FF" style={{marginRight: 10}} />
               <Text>나의 후기</Text>
             </View>
             <View style={{ justifyContent: 'center' }}>
@@ -54,8 +75,9 @@ const MyPage = (props) => {
           </View>
         </TouchableHighlight>
       </View>
-      {/* Render tracks that user has runned */}
-      <Text style={{marginLeft: 20, marginVertical: 10, fontSize: 18}}>내가 뛰었던 코스</Text>
+      <View style={{ padding: 5}}>
+        <Text style={{marginLeft: 20, fontSize: 25, fontWeight:'bold', color: 'white'}}>내가 뛰었던 코스</Text>
+      </View>
       <View>
         {dummyTrackRecord.map((trackRecord) => (
           <TrackRecord key={trackRecord.trackId} data={trackRecord} />
@@ -80,5 +102,3 @@ const mapDispatchToProps = (dispatch) => {
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(MyPage);
-
-const styles = StyleSheet.create({});
