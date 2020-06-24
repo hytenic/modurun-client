e5eb1e85d8d762822ab8bc05746387ba0af91a5f
monthem <lpoeh01@gmail.com>
2020-06-24 17:10:40 +0900

    MODIFY: appended types for API params and modified name for addToBookMark to toggleBookmark

--- components/modules/API/SG/messages.js
+++ components/modules/API/SG/messages.js
@@ -1,5 +1,9 @@
 import moduRunApiUtils from './utils';
 
+/**
+ * @param {Number} scheduleId
+ * @param {Number} page
+ */
 const getMessages = (scheduleId, page) => moduRunApiUtils.request('GET', `/messages/messages?start=${page}&scheduleId=${scheduleId}`);
 
 export default {
