ed7f1f388b967b72fd86da0a2e1e683fdb622ce6
monthem <lpoeh01@gmail.com>
2020-06-29 10:44:11 +0900

    MODIFY: added page for blank my tracks

--- components/modules/TrackList.js
+++ components/modules/TrackList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {
   StyleSheet, Text, View, StatusBar,
 } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
+import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler';
 import TrackListEntry from './TrackList/TrackListEntry';
 
 const anyOfKeyIsFalsy = (object, keys) => {
@@ -12,9 +12,20 @@ const anyOfKeyIsFalsy = (object, keys) => {
   return false;
 };
 
-const TrackList = ({ tracks, showBookmark, showAdd, getMyTracks }) => {
+const TrackList = ({ tracks, showBookmark, showAdd, getMyTracks, setTabIndex }) => {
+
+  const alertNoTrack = () => (
+    <View style={{ margin: 20, backgroundColor: 'white', elevation: 3, borderRadius: 10, height: 200, alignItems: 'center', justifyContent: 'center' }}>
+      <Text style={{ fontSize: 20, fontWeight: 'bold', color: 'grey', textAlign: 'center' }}>추가한 코스가 없습니다</Text>
+      <Text style={{ fontSize: 16, fontWeight: 'bold' }}>먼저 코스를 추가해볼까요?</Text>
+      <TouchableOpacity onPress={() => setTabIndex(1)} style={{padding: 10, backgroundColor: 'dodgerblue', borderRadius: 10, margin: 10, paddingHorizontal: 20 }}>
+        <Text style={{ color: 'white' }}>코스 찾기</Text>
+      </TouchableOpacity>
+    </View>
+  );
+
   if (!tracks) return <></>;
-  if (!tracks.length) return <></>;
+  if (!tracks.length) return alertNoTrack();
 
   const filteredTracks = tracks.filter((track) => !anyOfKeyIsFalsy(track, ['destination', 'origin', 'route']));
 
